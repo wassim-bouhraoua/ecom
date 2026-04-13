c70ec51e82615ea999b9e4130182f407a04d44d4
wassim-bouhraoua <wassimxtg@gmail.com>
2026-04-13 19:38:50 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() { //React component
       <p className="text-gray-500 mb-8">Browse our collection of products.</p>
 
       <Link href="/product" className="bg-black text-white px-6 py-3 rounded-lg hover:bg-gray-800 transition inline-block">
-        Go to Products
+        Go to Products (testing)
       </Link>
     </div>
   );
