b439c4eafb79c118c50a47ef01eefdef7d07f885
wassim-bouhraoua <wassimxtg@gmail.com>
2026-04-21 12:17:52 +0100

    fix?

--- app/components/InitProducts.tsx
+++ app/components/InitProducts.tsx
@@ -5,12 +5,8 @@ import { products } from "@/app/data/products";
 
 export default function InitProducts() {
   useEffect(() => {
-    const existing = localStorage.getItem("products");
-
-    if (!existing) {
-      localStorage.setItem("products", JSON.stringify(products));
-    }
+    localStorage.setItem("products", JSON.stringify(products));
   }, []);
 
-  return null; 
+  return null;
 }
